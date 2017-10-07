a1a27dc55a36cabbf90e07bd30fc5656a5118ef5
aaronstl <seavers.aaron@gmail.com>
2017-10-07 11:42:48 -0500

    Add files via upload

--- ZipCodeAS.cbl
+++ ZipCodeAS.cbl
@@ -1,92 +1,78 @@
-       IDENTIFICATION DIVISION.
-       PROGRAM-ID. ZipCodeAS.
-       AUTHOR.  Aaron Seavers.
-       CONFIGURATION SECTION.
- 
-       SPECIAL-NAMES.
-       CLASS ZipCodeClass IS "0" THRU "9", "A" THRU "Z", "a" Thru "z".
-       
-       DATA DIVISION.
-
-
-       WORKING-STORAGE SECTION.
-
-       
-       
-       01  ZipCode PIC X(5) VALUE ZERO.
-      *2 DAYS
-           88 StLouisArea       VALUE 63000 thru 63999. 
-           88 KansasCityArea       VALUE 64000 thru 64999.
-           88 ChicagoArea     VALUE 60000 thru 60999.
-      *3 Days
-           88 OutStateMissouri       VALUE 65000 thru 65999.
-           88 OutStateIllinois    VALUE 61000 thru 61999.
-           88 EastStLouis    VALUE 62000 thru 62999.
-      *4 Days    
-           88 OutsideBiStateArea   value 10000 thru 59999.
-           
-
-
-
-       PROCEDURE DIVISION.
-       Begin.
-       DISPLAY
-         "Enter a five digit zip code to find shipping times - "
-       
-       ACCEPT ZipCode
-       
-
-       if ZipCode is not numeric
-           if zipCode is ZipCodeClass
-               display "only enter numbers"
-               goback
-           else if zipCode is not zipCodeClass
-               display "you didnt enter enough numbers"
-      * so close!!!
-               goback
-               else
-                   display "You didnt enter enough values and you included a letter."
-               goback
-
-       end-if
-       end-if
-       end-if
-           
-        
-           
-       if OutsideBiStateArea 
-           display "You are from outside of the bi- state area"
-           Display " Your Zipcode " ZipCode " is 4 day shipping."
-       else if
-              OutStateMissouri
-                display "you are from -65 out state Missouri."
-                Display " Your Zipcode " ZipCode " is 3 day shipping."
-            else if 
-                   OutStateIllinois
-                     display "you are from -61 out state illinois"
-                     display "your zipcode " ZipCode " is 3 day shipping"
-                 else if
-                        EastStLouis
-                          display "you are from -62 east st louis"
-                          display "your zipcode " Zipcode " is 3 day shipping"
-                      else if
-                             stlouisarea
-                               DISPLAY "You are from the -63  St. Louis Area."
-                               Display " Your Zipcode " ZipCode " is 2 day shipping."
-                           else if
-                                  KansasCityArea
-                                    display "You are from the -64 Kansas City Area."
-                                   Display " Your Zipcode " ZipCode " is 2 day shipping."
-                                   else if ChicagoArea
-                                       Display "You are from the -60  Chicago Area."
-                                       Display " Your Zipcode " ZipCode " is 2 day shipping."
-                                       else
-                                           display "You did not enter a valid zip code. Please try again."
-                                   end-if
-                                end-if
-                           end-if
-                      end-if
-                 end-if
-            end-if
-       end-if
-       goback
+000100 IDENTIFICATION DIVISION.
+000200 PROGRAM-ID. ZipCodeAS.
+000300 AUTHOR.  Aaron Seavers.
+000400 CONFIGURATION SECTION.
+000500
+000600 SPECIAL-NAMES.
+000700 CLASS ZipCodeClass
+000800 IS "0" THRU "9","A" THRU "Z", "a" Thru "z".
+000900 
+001000 DATA DIVISION.
+001100
+001200 WORKING-STORAGE SECTION.
+001300 01  ZipCode PIC X(5) VALUE ZERO.
+001400*2 DAYS
+001500     88 StLouisArea     VALUE 63000 thru 63999. 
+001600     88 KansasCityArea  VALUE 64000 thru 64999.
+001700     88 ChicagoArea     VALUE 60000 thru 60999.
+001800*3 Days
+001900     88 OutStateMissouri    VALUE 65000 thru 65999.
+002000     88 OutStateIllinois    VALUE 61000 thru 61999.
+002100     88 EastStLouis         VALUE 62000 thru 62999.
+002200*4 Days    
+002300     88 OutsideBiStateArea   value 10000 thru 59999.
+002400     
+002500 PROCEDURE DIVISION.
+002600 Begin.
+002700 DISPLAY
+002800   "Enter a five digit zip code to find shipping times - "
+002900 
+003000 ACCEPT ZipCode
+003100 
+003200 if ZipCode is not numeric
+003300     if zipCode is ZipCodeClass
+003400         display "only enter numbers"
+003500         goback
+003600     else if zipCode is not zipCodeClass
+003700         display "you didnt enter enough numbers"
+003800         goback
+003900 end-if
+004000 end-if
+004100 end-if
+004200
+004300 if OutsideBiStateArea 
+004400     display "You are from outside of the bi- state area"
+004500     Display " Your Zipcode " ZipCode " is 4 day shipping."
+004600 else if
+004700    OutStateMissouri
+004800      display "you are from -65 out state Missouri."
+004900      Display " Your Zipcode " ZipCode " is 3 day shipping."
+005000      else if 
+005100        OutStateIllinois
+005200          display "you are from -61 out state illinois"
+005300          display "your zipcode " ZipCode " is 3 day shipping"
+005400           else if
+005500             EastStLouis
+005600              display "you are from -62 east st louis"
+005700              display "your zipcode " Zipcode " is 3 day shipping"
+005800                else if
+005900                 stlouisarea
+006000                   DISPLAY "You are from the -63  St. Louis Area."
+006100                   Display " Your Zipcode " ZipCode " is 2 day shipping."
+006200                     else if
+006300                       KansasCityArea
+006400                        display "You are from the -64 Kansas City Area."
+006500                         Display " Your Zipcode " ZipCode " is 2 day shipping."
+006600                          else if ChicagoArea
+006700                            Display "You are from the -60  Chicago Area."
+006800                            Display " Your Zipcode " ZipCode " is 2 day shipping."
+006900                             else
+007000                              display "You did not enter a valid zip code. Please try again."
+007100                             end-if
+007200                          end-if
+007300                     end-if
+007400                end-if
+007500           end-if
+007600      end-if
+007700 end-if
+007800 goback
